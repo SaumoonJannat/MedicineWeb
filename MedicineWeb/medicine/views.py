45491008b5e94b16223f6c4878edbf1b51f93853
Pariachowdhury <pariachowdhury950@gmail.com>
2024-04-27 04:54:24 +0600

    cart functionality added

--- MedicineWeb/medicine/views.py
+++ MedicineWeb/medicine/views.py
@@ -103,3 +103,20 @@ class updateAddress(View):
         else:
             messages.warning(request,"Invalid Input Data")
         return redirect("address")
+def add_to_cart(request):
+    user=request.user
+    product_id=request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user ,product=product).save()
+    return redirect("/cart")
+    pass
+
+def show_cart(request):
+    user=request.user
+    cart = Cart.objects.filter(user=user)
+    amount = 0
+    for p in cart:
+        value = p.quantity*p.product.discounted_price
+        amount = amount + value
+    totalamount = amount + 40
+    return render(request,'app/addtocart.html',locals())
