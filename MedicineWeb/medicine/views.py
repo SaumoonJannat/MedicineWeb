105df5a1506422f217e7bc7b321e3a507a18028a
Pariachowdhury <pariachowdhury950@gmail.com>
2024-04-27 04:53:56 +0600

    address added

--- MedicineWeb/medicine/views.py
+++ MedicineWeb/medicine/views.py
@@ -78,3 +78,28 @@ class ProfileView(View):
             messages.warning(request, "Invalid Input Data")
         return render(request, 'app/profile.html', locals())
 
+def address(request):
+    add=Customer.objects.filter(user=request.user)
+    return render(request, 'app/address.html',locals())
+
+class updateAddress(View):
+    def get(self,request,pk):
+        add = Customer.objects.get(pk=pk)
+        form = CustomerProfileForm(instance=add)
+        return render(request, 'app/updateAddress.html',locals())
+    def post(self,request,pk):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            add = Customer.objects.get(pk=pk)
+            add.name = form.cleaned_data['name']
+            add.locality = form.cleaned_data['locality']
+            add.city= form.cleaned_data['city']
+            add.mobile=form.cleaned_data['mobile']
+            add.state=form.cleaned_data['state']
+            add.zipcode = form.cleaned_data['zipcode']
+            add.save()
+            messages.success(request,"Congratulations! Profile Update Successful")
+
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return redirect("address")
