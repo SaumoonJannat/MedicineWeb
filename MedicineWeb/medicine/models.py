e1efaba02f98c3e310a488aaf3d1f20611198e28
Labanya23 <100686917+Labanya23@users.noreply.github.com>
2024-04-27 04:38:07 +0600

    product class added

--- MedicineWeb/medicine/models.py
+++ MedicineWeb/medicine/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    quantity = models.IntegerField(null=True)
+    description = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_image = models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
