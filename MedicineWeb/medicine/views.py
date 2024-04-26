cd107010843bd6500ce2c887facc5ee848abd4f8
Pariachowdhury <pariachowdhury950@gmail.com>
2024-04-27 04:53:23 +0600

    profileview added

--- MedicineWeb/medicine/views.py
+++ MedicineWeb/medicine/views.py
@@ -52,3 +52,29 @@ class CustomerRegistrationView(View):
         else:
             messages.warning(request, "Invalid input data")
         return render(request, 'app/customerregistration.html', locals())
+
+
+class ProfileView(View):
+    def get(self, request):
+        form = CustomerProfileForm()
+        return render(request, 'app/profile.html', locals())
+
+    def post(self, request):
+        form = CustomerProfileForm(request.POST)
+        if form.is_valid():
+            user = request.user
+            name = form.cleaned_data['name']
+            locality = form.cleaned_data['locality']
+            city = form.cleaned_data['city']
+            mobile = form.cleaned_data['mobile']
+            state = form.cleaned_data['state']
+            zipcode = form.cleaned_data['zipcode']
+
+            reg = Customer(user=user, name=name, locality=locality, mobile=mobile, city=city, state=state,
+                           zipcode=zipcode)
+            reg.save()
+            messages.success(request, "Congratulations! Profile Saved Successfully.")
+        else:
+            messages.warning(request, "Invalid Input Data")
+        return render(request, 'app/profile.html', locals())
+
