fd00f62f691f0fda934951c16dfc588b7fd298fb
Labanya23 <100686917+Labanya23@users.noreply.github.com>
2024-04-27 17:33:20 +0600

    cart class  added checkout view add payment add Orderplace add

--- MedicineWeb/medicine/models.py
+++ MedicineWeb/medicine/models.py
@@ -3,24 +3,23 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 STATE_CHOICES = (
-    ('Dhaka','Dhaka'),
-    ('Rangpur','Rangpur'),
-    ('Chattogram','Chattogram'),
-    ('Rajshai','Rajshai'),
-    ('Khulna','Khulna'),
-    ('Barishal','Barishal'),
-    ('Mymensingh','Mymensingh')
+    ('Dhaka', 'Dhaka'),
+    ('Rangpur', 'Rangpur'),
+    ('Chattogram', 'Chattogram'),
+    ('Rajshai', 'Rajshai'),
+    ('Khulna', 'Khulna'),
+    ('Barishal', 'Barishal'),
+    ('Mymensingh', 'Mymensingh')
 )
 
-
 CATEGORY_CHOICES = (
-    ('CL','Cold'),
-    ('EY','Eye'),
-    ('DT','Dehydration'),
-    ('HT','Heat Stroke'),
-    ('SA','Seasonal allergies'),
-    ('TP','Typhoid'),
-    ('OT','Other')
+    ('CL', 'Cold'),
+    ('EY', 'Eye'),
+    ('DT', 'Dehydration'),
+    ('HT', 'Heat Stroke'),
+    ('SA', 'Seasonal allergies'),
+    ('TP', 'Typhoid'),
+    ('OT', 'Other')
 )
 
 
@@ -30,29 +29,64 @@ class Product(models.Model):
     discounted_price = models.FloatField()
     quantity = models.IntegerField(null=True)
     description = models.TextField(default='')
-    category = models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     product_image = models.ImageField(upload_to='product')
 
     def __str__(self):
         return self.title
 
+
 class Customer(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     locality = models.CharField(max_length=200)
     city = models.CharField(max_length=50)
     mobile = models.IntegerField(default=0)
     zipcode = models.IntegerField()
-    state = models.CharField(choices=STATE_CHOICES,max_length=200)
+    state = models.CharField(choices=STATE_CHOICES, max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Cart(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+
 
 @property
 def total_cost(self):
-    return self.quantity*self.product.discounted_price
+    return self.quantity * self.product.discounted_price
+
+
+STATUS_CHOICES = (
+    ('accpected', 'accpected'),
+    ('packed', 'packed'),
+    ('On the way', 'on the way'),
+    ('Deliverd', 'Deliverd'),
+    ('pending', 'pending'),
+)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    bikaspay_order_id = models.CharField(max_length=100, blank=True, null=True)
+    bikaspay_payment_status = models.CharField(max_length=100, blank=True, null=True)
+    bikas_payment_id = models.CharField(max_length=100, blank=True, null=True)
+    paid = models.BooleanField(default=False)
+
+
+class OrderPlace(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, default="")
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product_discounted_price
